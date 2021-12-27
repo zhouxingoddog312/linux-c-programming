03f56a2ac7d50bc72ae4fea05ac0bfa4808a5563
zhouxin <zhouxin312@qq.com>
2021-12-27 21:35:40 +0800

    2021年 12月 27日 星期一 21:35:40 CST

---
+++ chapter-11/fork2.c
@@ -0,0 +1,32 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+int main(void)
+{
+	pid_t pid;
+	char *message;
+	int n;
+	printf("fork program starting\n");
+	pid=fork();
+	switch(pid)
+	{
+		case -1:
+			perror("fork failed");
+			break;
+		case 0:
+			message="This is the child";
+			n=2;
+			break;
+		default:
+			message="This is the parent";
+			n=5;
+			break;
+	}
+	for(;n>0;n--)
+	{
+		puts(message);
+		sleep(1);
+	}
+	exit(0);
+}
