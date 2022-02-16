6bfcb6132ae363a5a4dd005b845497c0b71c5609
zhouxin <zhouxin312@qq.com>
2022-02-16 13:52:47 +0800

    2022年 02月 16日 星期三 13:52:47 CST at home

--- chapter-12/thread8a.c
+++ chapter-12/thread8a.c
@@ -12,13 +12,12 @@ int main(void)
 	int lots_of_threads;
 	for(lots_of_threads=0;lots_of_threads<NUM_THREADS;lots_of_threads++)
 	{
-		res=pthread_create(&a_thread[lots_of_threads],NULL,thread_function,(void *)&lots_of_threads);
+		res=pthread_create(&a_thread[lots_of_threads],NULL,thread_function,(void *)lots_of_threads);
 		if(res!=0)
 		{
 			perror("Thread creation failed");
 			exit(EXIT_FAILURE);
 		}
-		sleep(1);
 	}
 	printf("Waiting for threads to finish...\n");
 	for(lots_of_threads=NUM_THREADS-1;lots_of_threads>=0;lots_of_threads--)
@@ -36,7 +35,7 @@ int main(void)
 }
 void *thread_function(void *arg)
 {
-	int my_number=*(int *)arg;
+	int my_number=(int)arg;
 	int rand_num;
 	printf("thread_function is running. Argument was %d\n",my_number);
 	rand_num=1+(int)(9.0*rand()/(RAND_MAX+1.0));
