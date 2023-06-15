38575e75cd56277bf387ccb125d1a6ab1ae79e3f
zhouxin <zhouxin312@qq.com>
2023-06-15 14:46:25 +0800

    routine

--- cplus/chapter-13/13-44/String.cpp
+++ cplus/chapter-13/13-44/String.cpp
@@ -1,1 +1,80 @@
-static std::allocator<char> alloc;
+#include <memory>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include "String.h"
+
+String::String(const char *str)
+{
+	size_t n=strlen(str);
+	std::pair<char *,char *> newdata=alloc_n_copy(str,str+n);
+	head=newdata.first;
+	tail=cap=newdata.second;
+}
+String::String(const String &s)
+{
+	std::pair<char *,char *> newdata=alloc_n_copy(s.begin(),s.end());
+	head=newdata.first;
+	tail=cap=newdata.second;
+}
+String & String::operator=(const String &rhs)
+{
+	std::pair<char *,char *> newdata=alloc_n_copy(rhs.begin(),rhs.end());
+	free();
+	head=newdata.first;
+	tail=cap=newdata.second;
+	return *this;
+}
+String String::operator+(const String &rhs)
+{
+	String tmp(*this);
+	size_t n=rhs.size();
+	tmp.chk_n_alloc(n);
+	char *dest=tmp.tail;
+	char *src=rhs.begin();
+	for(size_t i=0;i!=6;++i)
+		alloc.construct(dest++,*src++);
+	tmp.tail=dest;
+	return tmp;
+}
+
+std::allocator<char> String::alloc;
+void String::free()
+{
+	if(head)
+	{
+		std::for_each(begin(),end(),[](char &c) {alloc.destroy(&c);});
+		alloc.deallocate(head,cap-head);
+	}
+}
+std::pair<char *,char *> String::alloc_n_copy(const char *b,const char *e)
+{
+	char *newdata=alloc.allocate(e-b);
+	return {newdata,std::uninitialized_copy(b,e,newdata)};
+}
+void String::reallocate(size_t n)
+{
+	size_t newcap=(size()>n?size()*2:n*2);
+	char *newdata=alloc.allocate(newcap);
+	char *dest=newdata;
+	char *src=head;
+	for(size_t i=0;i!=size();++i)
+		alloc.construct(dest++,std::move(*src++));
+	free();
+	head=newdata;
+	tail=dest;
+	cap=head+newcap;
+}
+std::istream & operator>>(std::istream &in,String &str)
+{
+	std::string tmp;
+	in>>tmp;
+	String Stmp(tmp.c_str());
+	str=Stmp;
+	return in;
+}
+std::ostream & operator<<(std::ostream &out,const String &str)
+{
+	std::for_each(str.begin(),str.end(),[&out](const char &c)->void {out<<c;});
+	return out;
+}
--- cplus/chapter-13/13-44/String.h
+++ cplus/chapter-13/13-44/String.h
@@ -1,15 +1,35 @@
-#ifndef _STRING_H
-#define _STRING_H
+#ifndef _sTRING_H
+#define _sTRING_H
+#include <iostream>
+#include <utility>
 class String
 {
+	friend std::istream & operator>>(std::istream &in,String &str);
+	friend std::ostream & operator<<(std::ostream &out,const String &str);
 	public:
-		String();
+		String():head(nullptr),tail(nullptr),cap(nullptr) {}
 		String(const char *);
+		String(const String &);
+		String & operator=(const String &);
+		String operator+(const String &);
+		~String() {free();}
+
+		size_t size() const {return tail-head;}
+		size_t capacity() const {return cap-head;}
+		bool empty() const {return (head==tail)?true:false;}
+		char * begin() const {return head;}
+		char * end() const {return tail;}
 	private:
 		char *head;
 		char *tail;
 		char *cap;
 		static std::allocator<char> alloc;
-		void chk_n_alloc(size_t n) {if(n>capacity()) reallocate();}
+		void free();
+
+		std::pair<char *,char *> alloc_n_copy(const char *,const char *);
+		void chk_n_alloc(size_t n) {if(n+size()>capacity()) reallocate(n);}
+		void reallocate(size_t n);
 };
+std::istream & operator>>(std::istream &in,String &str);
+std::ostream & operator<<(std::ostream &out,const String &str);
 #endif
