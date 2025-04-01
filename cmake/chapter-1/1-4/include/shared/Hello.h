8cab920cf168bb77e9af7d0f5954e7500324ce37
goddog312 <zhouxin312@qq.com>
2025-04-01 21:54:02 +0800

    routing

---
+++ cmake/chapter-1/1-4/include/shared/Hello.h
@@ -0,0 +1,8 @@
+#ifndef HELLO_H
+#define HELLO_H
+class Hello
+{
+	public:
+		void print();
+};
+#endif
---
+++ cmake/chapter-1/installing/src/Hello.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "installing/Hello.h"
+void Hello::print()
+{
+	std::cout<<"Hello Install!"<<std::endl;
+}
