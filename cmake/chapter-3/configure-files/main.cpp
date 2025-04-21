1941c9b6bca810aea12433d788bcc1a39cad1fb0
zhouxin <zhouxin312@qq.com>
2025-04-21 22:36:01 +0800

    routine

---
+++ cmake/chapter-3/configure-files/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "ver.h"
+#include "path.h"
+int main(void)
+{
+	std::cout<<"Hello Version "<<ver<<"!"<<std::endl;
+	std::cout<<"Paht is "<<path<<std::endl;
+	return 0;
+}
